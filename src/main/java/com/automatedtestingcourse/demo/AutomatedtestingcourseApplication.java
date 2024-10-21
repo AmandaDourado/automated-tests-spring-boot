0dfb1edee608df5b45187453e122cb0294a0e811
Amanda Dourado <amanda_lopes1998@hotmail.com>
2024-10-21 19:44:14 -0300

    create api planets

---
+++ src/main/java/com/automatedtestingcourse/demo/AutomatedtestingcourseApplication.java
@@ -0,0 +1,13 @@
+package com.automatedtestingcourse.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedtestingcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedtestingcourseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/automatedtestingcourse/demo/domain/PlanetService.java
@@ -0,0 +1,18 @@
+package com.automatedtestingcourse.demo.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlanetService {
+
+    private PlanetRepository planetRepository;
+
+    public PlanetService(PlanetRepository planetRepository) {
+        this.planetRepository = planetRepository;
+    }
+
+    public Planet create(Planet planet) {
+        return planetRepository.save(planet);
+    }
+}
